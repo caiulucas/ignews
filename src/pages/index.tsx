063bb148099d49afdbf9c3259cf1bc802a1de8cb
Caio Lucas <lienscarlet@gmail.com>
2021-03-23 03:23:44 -0300

    chore: global styled defined

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Inicio | ig.news</title>
+    </Head>
+
+    <h1>Hello, World!</h1>
+  </>
+);
+
+export default Home;
