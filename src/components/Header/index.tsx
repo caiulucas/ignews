7412b5b5153ebf460aa83d05be414d7238fb93ab
Caio Lucas <lienscarlet@gmail.com>
2021-03-23 08:12:56 -0300

    feat: homePage created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignInbutton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export const Header: React.FC = () => (
+  <header className={styles.headerContainer}>
+    <div className={styles.headerContent}>
+      <img src="/images/logo.svg" alt="ig.news" />
+      <nav>
+        <a className={styles.active} href="https://localhost:3000">
+          Home
+        </a>
+        <a href="https://localhost:3000">Posts</a>
+      </nav>
+      <SignInbutton />
+    </div>
+  </header>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from "next/app";
+import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
 
 export default MyApp;
